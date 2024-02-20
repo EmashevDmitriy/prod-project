4ba60a60d0b3875f76f04c072fe5fde02c87a8c2
EmashevDmitriy <emashev_dmitrit1@mail.ru>
2024-02-20 15:34:25 +0700

    add error boundary, not found page, loader

--- src/app/providers/routers/ui/AppRouter.tsx
+++ src/app/providers/routers/ui/AppRouter.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/configRouter/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
+		<Suspense fallback={<PageLoader />}>
 			<div className="page-wrapper">
 				<Routes>
 					{Object.values(routeConfig).map(({ element, path }) => (
