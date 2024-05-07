01a355e785d933a773bd7f37b614ec04b84d1546
EmashevDmitriy <emashev_dmitrit1@mail.ru>
2024-05-07 14:28:57 +0700

    production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins({
 	paths,
@@ -23,6 +24,9 @@ export function buildPlugins({
 			__IS_DEV__: JSON.stringify(isDev),
 			__API__: JSON.stringify(apiUrl),
 			__PROJECT__: JSON.stringify(project),
+		}),
+		new CopyPlugin({
+			patterns: [{ from: paths.locales, to: paths.buildLocales }],
 		}),
 	];
 
